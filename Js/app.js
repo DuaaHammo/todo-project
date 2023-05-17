29775d67236d3b2327603d93d2cbac1c6461f7a3
Duaahammo <Duaahammo@icloud.com>
2023-05-17 19:27:32 +0000

    update

--- Js/app.js
+++ Js/app.js
@@ -1,22 +1,59 @@
-//* Name */
-
-let userName = prompt("Please enter your name:");
-let gender = prompt ("Please enter your gender (male/female)")
 
 
-/* Age */
-let age = parseInt(prompt("Please enter your age:"));
+function welcomeMassage(){
+  var username = prompt("Please enter your name:");
+    //gender
+    var gender = prompt("Please enter your gender (male/female)");
+    // Mr/Ms
+    var title = "";
+    if (gender === "male") {
+      title = "Mr.";
+    } else if (gender === "female") {
+      title = "Ms.";
+    }
+    var age = parseInt(prompt("Please enter your age:"));
+
+// Gender
+
 
 if (age <= 0) {
   alert("Invalid age! Age must be greater than zero.");
 } else {
   alert("Valid age. Thank you!");
 }
+  
+// Ask the user to confirm if he/she wants to skip the welcoming message.
+
+  var skipMassaege =prompt("Do you want to skip the welcoming message?");
+  if (skipMassaege == "no") {
+  alert("Welcome, " + title + " " + username + "!");
+  }
+  
+
+}
 
 
-//* Welcome Message */
+//* */
 
-/* Mr & Ms */
+function qustions(){
+
+var q1=prompt("Do you love ASAC?");
+var q2 =prompt("Are you here?");
+var q3=prompt("Do you love Bayan?");
+
+var answer=[q1,q2,q3]
+
+for(let i=0; i< answer.length; i++){
+
+if (answer[i]==''){
+  answer[i]="invalid"
+   
+}
+  
+}
+console.log(answer)
+}
 
 
-
+welcomeMassage()
+qustions()
