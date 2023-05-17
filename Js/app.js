977923f872762aad5a1895ef294b710482d94b0e
Duaahammo <Duaahammo@icloud.com>
2023-05-17 07:45:12 +0000

    update

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,7 @@
 //* Name */
 
 let userName = prompt("Please enter your name:");
-console.log("Hello, " + userName + "!");
+let gender = prompt ("Please enter your gender (male/female)")
 
 
 /* Age */
@@ -16,29 +16,7 @@ if (age <= 0) {
 
 //* Welcome Message */
 
-let skipMessage = confirm("Do you want to skip the welcoming message?");
-
-if (skipMessage) {
-  console.log("Skipping the welcoming message.");
-} else {
-  console.log("Welcome to our website!");
-}
-
 /* Mr & Ms */
-let username = prompt("Please enter your name:");
-let userGender = prompt("Please enter your gender (male/female):");
-let welcomemessage;
 
-if (userGender.toLowerCase() === "male") {
-  welcomemessage = "Welcome, Mr " + username + "!";
-} else if (userGender.toLowerCase() === "female") {
-  welcomemessage = "Welcome, Ms " + username + "!";
-} else {
-  welcomemessage = "Welcome, " + username + "!";
-}
 
-let skipmessage = confirm("Do you want to skip the welcoming message?");
 
-if (!skipmessage) {
-  alert(welcomemessage);
-}
