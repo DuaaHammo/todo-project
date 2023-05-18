03147ee229939996c83a3cde23beaf7883d111ec
Duaahammo <Duaahammo@icloud.com>
2023-05-18 15:57:01 +0000

    feedback

--- Js/app.js
+++ Js/app.js
@@ -1,59 +1,61 @@
+var username = prompt("Please enter your name:");
+var gender = ""
+var title = "";
+var answers=[]
 
-
-function welcomeMassage(){
-  var username = prompt("Please enter your name:");
-    //gender
-    var gender = prompt("Please enter your gender (male/female)");
-    // Mr/Ms
-    var title = "";
-    if (gender === "male") {
-      title = "Mr.";
-    } else if (gender === "female") {
-      title = "Ms.";
-    }
-    var age = parseInt(prompt("Please enter your age:"));
-
-// Gender
-
-
-if (age <= 0) {
-  alert("Invalid age! Age must be greater than zero.");
-} else {
-  alert("Valid age. Thank you!");
-}
-  
-// Ask the user to confirm if he/she wants to skip the welcoming message.
-
-  var skipMassaege =prompt("Do you want to skip the welcoming message?");
-  if (skipMassaege == "no") {
-  alert("Welcome, " + title + " " + username + "!");
+function selectGender() {
+   gender = prompt("Please enter your gender (male/female):");
+  if (gender === "male") {
+    title = "Mr.";
+  } else if (gender === "female") {
+    title = "Ms.";
   }
-  
+}
+
+function checkAge() {
+
+  var age = parseInt(prompt("Please enter your age:"));
+
+  if (age <= 0) {
+    alert("Invalid age! Age must be greater than zero.");
+  } else {
+    alert("Valid age. Thank you!");
+  }
 
 }
 
+function welcomeMessage(){
+  var skipMessage = confirm("Do you want to skip the welcoming message?");
 
-//* */
+  if (!skipMessage) {
+    alert("Welcome, " + title + " " + username + "!");
+  }
 
-function qustions(){
-
-var q1=prompt("Do you love ASAC?");
-var q2 =prompt("Are you here?");
-var q3=prompt("Do you love Bayan?");
-
-var answer=[q1,q2,q3]
-
-for(let i=0; i< answer.length; i++){
-
-if (answer[i]==''){
-  answer[i]="invalid"
-   
 }
-  
-}
-console.log(answer)
+
+function questions(){
+var q1 = prompt("Do you love ASAC?");
+var q2 = prompt("Are you here?");
+var q3 = prompt("Do you love Bayan?");
+
+answers = [q1, q2, q3];
 }
 
 
-welcomeMassage()
-qustions()
+function displayAnswers() {
+
+  for (let i = 0; i < answers.length; i++) {
+
+    if (answers[i] === "") {
+      answers[i] = "invalid";
+    }
+    console.log(answers[i]);
+
+  }
+
+}
+selectGender()
+checkAge()
+welcomeMessage();
+questions();
+displayAnswers();
